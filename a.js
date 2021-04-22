9a83a7e0b03c448574bfc701ba057251d2f3cab7
Oğuzhan ÇART <54273323+oguzhan18@users.noreply.github.com>
2021-04-22 09:57:52 +0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,167 @@
+var audio = new Audio('http://yayin2.yayindakiler.com:4112/;');
+var button = document.getElementById('play')
+var playing = false
+
+button.addEventListener('click', () => {
+  playing = !playing
+  console.log(audio)
+  
+  if(playing) {
+    console.log(audio.currentTime)
+    audio.play()    
+  } else {
+    audio.pause()
+  }
+})   
+
+      
+       var audio1 = new Audio(' http://yayin2.canliyayin.org:8698/;');
+       var button = document.getElementById('play1')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio1)
+         
+         if(playing) {
+           console.log(audio1.currentTime)
+           audio1.play()    
+         } else {
+           audio1.pause()
+         }
+       })
+       var audio2 = new Audio('http://yayin2.yayindakiler.com:4188/;');
+       var button = document.getElementById('play2')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio2)
+         
+         if(playing) {
+           console.log(audio2.currentTime)
+           audio2.play()    
+         } else {
+           audio2.pause()
+         }
+       })
+       var audio3 = new Audio('http://powerturkeniyiler.listenpowerapp.com/powerturkeniyiler/mpeg/icecast.audio');
+       var button = document.getElementById('play3')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio3)
+         
+         if(playing) {
+           console.log(audio3.currentTime)
+           audio3.play()    
+         } else {
+           audio3.pause()
+         }
+       })
+       var audio4 = new Audio('http://46.20.7.116/;stream.mp3');
+       var button = document.getElementById('play4')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio4)
+         
+         if(playing) {
+           console.log(audio4.currentTime)
+           audio4.play()    
+         } else {
+           audio4.pause()
+         }
+       }) 
+       var audio5 = new Audio('http://powerturkefsane.listenpowerapp.com/powerturkefsane/mpeg/icecast.audio');
+       var button = document.getElementById('play5')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio5)
+         
+         if(playing) {
+           console.log(audio5.currentTime)
+           audio5.play()    
+         } else {
+           audio5.pause()
+         }
+       }) 
+       var audio6 = new Audio('http://37.247.98.8/stream/30/');
+       var button = document.getElementById('play6')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio6)
+         
+         if(playing) {
+           console.log(audio6.currentTime)
+           audio6.play()    
+         } else {
+           audio6.pause()
+         }
+       })
+       var audio7 = new Audio('http://yayin2.yayindakiler.com:4064/;stream/1');
+       var button = document.getElementById('play7')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio7)
+         
+         if(playing) {
+           console.log(audio7.currentTime)
+           audio7.play()    
+         } else {
+           audio7 .pause()
+         }
+       }) 
+       var audio8 = new Audio('http://37.247.98.8/stream/21/');
+       var button = document.getElementById('play8')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio8)
+         
+         if(playing) {
+           console.log(audio8.currentTime)
+           audio8.play()    
+         } else {
+           audio8 .pause()
+         }
+       }) 
+       var audio9 = new Audio(' http://37.247.98.8/stream/166/');
+       var button = document.getElementById('play9')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio9)
+         
+         if(playing) {
+           console.log(audio9.currentTime)
+           audio9.play()    
+         } else {
+           audio9 .pause()
+         }
+       })
+       var audio10 = new Audio('http://37.247.98.8/stream/25/');
+       var button = document.getElementById('play10')
+       var playing = false
+       
+       button.addEventListener('click', () => {
+         playing = !playing
+         console.log(audio10)
+         
+         if(playing) {
+           console.log(audio10.currentTime)
+           audio10.play()    
+         } else {
+           audio10 .pause()
+         }
+       })
